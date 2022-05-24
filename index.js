df1cdef0d9ab5d86cbf2b89fec2aef8c1fad8061
Unletical <71699372+Unletical@users.noreply.github.com>
2022-05-24 13:23:09 +0300

    dag 1

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+const db_name = path.join(__dirname, "data", "apptest.db");
 
+const { listen } = require("express/lib/application");
+
+// gör en variabel så man kan använda express
 const app = express();
 
-app.listen(3000, () =>  {
-  console.log("Server started");
-});
 
-app.get("/", (req, res) =>  {
-  res.send ("Hello world...");
-});
+// Kod för för vilken motorn man använder för att få saker att visas
+app.set("view engine", "ejs");
+
+// Allt som ska synnas på sidan ska vara i views mappen
+app.set("views", path.join(__dirname, "views"));
+
+// 
+app.use(express.static(path.join(__dirname, "public")));
+
+// Connectar till databasen
+const db = new sqlite3.Database(db_name, err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Successful connection to the database 'texter.db'");
+  });
+
+
+  app.get("/", (req, res) => { {
+    // Response att visa det som finns innuti index.ejs i views mappen
+    res.render("index");
+  }});
+
+  app.listen(3000, () => { 
+    console.log("Server started");
+})
